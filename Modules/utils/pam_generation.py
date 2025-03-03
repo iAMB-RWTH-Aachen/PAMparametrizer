3965c8d6dc7215e15d6f773b89681b947cbb5a86
samira.vandenbogaard <samira.vandenbogaard@gmail.com>
2025-03-03 15:07:36 +0100

    updated links to parameter files

--- Modules/utils/pam_generation.py
+++ Modules/utils/pam_generation.py
@@ -222,7 +222,7 @@ def set_up_yeast_config():
 
 def setup_pputida_pam(pam_info_file:str= os.path.join(
                                              'Results', '1_preprocessing',
-                                             'proteinAllocationModel_iJN1463_EnzymaticData_250117.xlsx'),
+                                             'proteinAllocationModel_iJN1463_EnzymaticData_250225.xlsx'),
                      model:str = 'Models/iJN1463.xml',
                      total_protein: Union[bool, float] = 0.3, active_enzymes: bool = True,
                     translational_enzymes: bool = True, unused_enzymes: bool = True, sensitivity = True):
@@ -239,7 +239,7 @@ def setup_pputida_pam(pam_info_file:str= os.path.join(
 
 def setup_cglutanicum_pam(pam_info_file:str= os.path.join(
                                              'Results', '1_preprocessing',
-                                             'proteinAllocationModel_iCGB21FR_EnzymaticData_250214.xlsx'),
+                                             'proteinAllocationModel_iCGB21FR_EnzymaticData_250227.xlsx'),
                      model:str = 'Models/iCGB21FR_annotated_copyable.xml',
                      total_protein: Union[bool, float] = 0.3, active_enzymes: bool = True,
                     translational_enzymes: bool = True, unused_enzymes: bool = True, sensitivity = True):
