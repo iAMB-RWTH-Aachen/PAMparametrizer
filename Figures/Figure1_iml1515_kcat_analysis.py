b998e319aa407e55c9a6e3f19805d50940574754
samira.vandenbogaard <samira.vandenbogaard@gmail.com>
2025-03-12 17:46:40 +0100

    started making scripts for generating figures

---
+++ Figures/Figure1_iml1515_kcat_analysis.py
@@ -0,0 +1,376 @@
+import matplotlib.pyplot as plt
+from matplotlib.colors import to_hex
+import matplotlib.cm as cm
+import matplotlib.colors as mcolors
+from matplotlib import gridspec
+import numpy as np
+import pandas as pd
+import os
+from typing import List, Dict, Tuple
+from PAModelpy.utils import set_up_pam
+
+from Scripts.i2_parametrization.pam_parametrizer_iML1515 import set_up_pamparametrizer
+
+from Modules.utils.pam_generation import (get_rxn2kcat_protein2gene_dict,
+                                          _extract_reaction_id_from_catalytic_reaction_id,
+                                          _get_rxn2kcat_as_series,
+                                          create_pamodel_from_diagnostics_file
+                                         )
+from Modules.utils.pamparametrizer_analysis import (calculate_kcat_differences,
+                                                    plot_histogram_logspace,
+                                                    select_clustered_rows_by_variation,
+                                                    get_clusters_from_clustermap,
+                                                    set_up_pam_parametrizer_and_get_substrate_uptake_rates
+                                                   )
+
+from Modules.utils.pamparametrizer_visualization import plot_valid_data, plot_simulation
+
+
+COG_MAPPER = {'Amino acid transport and metabolism': 'Amino acid metabolism',
+       'Carbohydrate transport and metabolism': 'Carbon metabolism',
+       'Cell cycle control, cell division, chromosome partitioning': 'Cell cycle',
+       'Cell wall/membrane/envelope biogenesis':"Cell membrane biogenesis",
+       'Coenzyme transport and metabolism': 'Coenzyme metabolism',
+              'Defense mechanisms':'Defense mechanisms',
+       'Energy production and conversion': 'Energy generation', 'Function unknown': 'Function unknown',
+       'General function prediction only': 'General function',
+       'Inorganic ion transport and metabolism': 'Inorganic ion metabolism',
+       'Intracellular trafficking, secretion, and vesicular transport': 'Intracellular transport',
+       'Lipid transport and metabolism': 'Lipid metabolism',
+       'Nucleotide transport and metabolism': 'Nucleotide metabolism',
+       'Posttranslational modification, protein turnover, chaperones':'Protein modification',
+       'Replication, recombination and repair': 'Replication',
+       'Secondary metabolites biosynthesis, transport and catabolism': 'Secondary metabolites metabolism',
+       'Signal transduction mechanisms': 'Signal transduction', 'Transcription': 'Transcription',
+       'Translation, ribosomal structure and biogenesis': 'Translation'}
+
+
+def gaussian(x, mean, amplitude, standard_deviation):
+    return amplitude * np.exp( - (x - mean)**2 / (2*standard_deviation ** 2))
+
+def calculate_distribution_statistics(bin_heigths: list[float],
+                                      bin_borders:list[float]) -> None:
+    peak_bin_index = np.argmax(bin_heigths)
+    peak_bin_value = (bin_borders[peak_bin_index] + bin_borders[peak_bin_index + 1]) / 2
+    area_under_curve = sum(
+        [(abs(bin_borders[i]) - abs(bin_borders[i + 1])) * bin_heigths[i] for i in range(len(bin_heigths))])
+
+    print(
+        f'\tMost frequent flux:\t\t{peak_bin_value} mmol/gCDW/h\n\tArea under the curve:\t\t{area_under_curve} mmol/gCDW/h\n')
+
+def create_kcat_histogram_old_vs_new(data_file_paths: list[pd.DataFrame],
+                                     label_names:list[str],
+                                     cumulative: bool = False,
+                                     other_colors = {'GotEnzymes': 'grey', 'After preprocessing': 'black'},
+                                     legend = True):
+    fig, ax = plt.subplots()
+    n_bins = 50
+    i = 0
+    cmap = plt.get_cmap("coolwarm")
+
+    for label, data_file_path in zip(label_names, data_file_paths):
+        aes_parameter_df = pd.read_excel(data_file_path, sheet_name='ActiveEnzymes')
+        kcat_values = aes_parameter_df.kcat_values.dropna()
+        print('------------------------------------------------------------------------')
+        print(f'The kcat set from {label} has:\n \tMedian:\t\t\t{np.median(kcat_values)} \n \tMean:\t\t\t{np.mean(kcat_values)}')
+
+
+        hist, bins = np.histogram(kcat_values, bins=n_bins)
+        logbins = np.logspace(np.log10(bins[0]), np.log10(bins[-1]), len(bins))
+
+        if label in other_colors.keys():
+            color = other_colors[label]
+        else:
+            i += 1
+            color = to_hex(cmap(i/ (len(data_file_paths)-len(other_colors))))
+        bin_heights, bin_borders, _ = ax.hist(kcat_values, bins = logbins, histtype='step',
+                                              stacked=True, fill=False, label= label, color=color, cumulative=cumulative)
+        calculate_distribution_statistics(bin_heights, bin_borders)
+
+    ax.vlines([13.7], 0, 1e4, linestyles='dotted')
+
+    plt.yscale('log')
+    plt.ylabel('Frequency')
+    plt.xscale('log')
+    plt.xlabel('Kcat value [s-1]')
+    if legend:
+        plt.legend()
+    plt.tight_layout()
+    return fig, ax
+
+def create_kcat_change_per_cog_barplot(original_pam_kcat_file: str,
+                                       model_file: str,
+                                       diagnostic_files: List[str]):
+    individual_info_with_cog = get_kcat_changes_per_cog(original_pam_kcat_file,
+                                       model_file,
+                                       diagnostic_files)
+    return create_barplot(individual_info_with_cog)
+
+def get_kcat_changes_per_cog(original_pam_kcat_file:str,
+                             model_file:str,
+                             diagnostic_files:List[str])->pd.DataFrame:
+    #extract kcat at starting position of optimization
+    rxn2kcat, _ = get_rxn2kcat_protein2gene_dict(original_pam_kcat_file, model_file)
+
+    best_individual_df = merge_all_diagnostic_files(diagnostic_files)
+    best_individual_df = calculate_change_in_kcat(best_individual_df,
+                                                  rxn2kcat)
+    best_individual_with_cog = add_pathway_annotation_to_kcat_changes(best_individual_df)
+    best_individual_cog_summarized= summarize_and_pivot_cog_info_df_to_long(best_individual_with_cog)
+    return summarize_and_pivot_cog_info_df_to_long(best_individual_cog_summarized)
+
+
+def merge_all_diagnostic_files(diagnostic_files: List[str]) -> pd.DataFrame:
+    best_individual_df = None
+
+    for i, file in enumerate(diagnostic_files):
+        pam_param_results = pd.read_excel(file, sheet_name='Best_Individuals')
+        pam_param_error = pd.read_excel(file, sheet_name='Final_Errors')
+        pam_param_results = pam_param_results.merge(pam_param_error, on='run_id', how='left')
+        pam_param_results['alternative'] = i + 1
+
+        if best_individual_df is None:
+            best_individual_df = pam_param_results
+        else:
+            best_individual_df = pd.concat([best_individual_df, pam_param_results])
+    return best_individual_df
+
+def calculate_change_in_kcat(best_individual_df: pd.DataFrame,
+                             rxn2kcat: Dict[str,dict]):
+    best_individual_df['rxn_id'] = best_individual_df['rxn_id'].apply(lambda row:
+                                                                      _extract_reaction_id_from_catalytic_reaction_id(
+                                                                          row
+                                                                      ))
+    return best_individual_df.groupby(
+        'alternative', group_keys=True).apply(calculate_kcat_differences, rxn2kcat).reset_index(drop=True)
+
+def add_pathway_annotation_to_kcat_changes(best_individual_df: pd.DataFrame,
+                              pathway_annotation_file:str = os.path.join('Data', 'GeneList_ecoli.xlsx'),
+                              gene_to_protein_sheet:str = 'GeneList', rxn_to_gene_sheet: str = 'gene2rxn'
+                                           )->pd.DataFrame:
+    # get pathway information from genelist file
+    gene_list_info = pd.read_excel(pathway_annotation_file, sheet_name=gene_to_protein_sheet)
+    rxn2gene_info = pd.read_excel(pathway_annotation_file, sheet_name=rxn_to_gene_sheet)
+
+    # map cog information to reactions using the gene id
+    gene2rxn2cog = pd.merge(gene_list_info[['bnumber', 'COG description']], rxn2gene_info,
+                            left_on='bnumber', right_on='Gene', how='inner')
+    gene2rxn2cog.Reactions = gene2rxn2cog.Reactions.str.split(', ')
+    gene2rxn2cog['COG description'] = gene2rxn2cog['COG description'].str.split(';')
+    gene2rxn2cog = gene2rxn2cog.explode('Reactions')
+    gene2rxn2cog = gene2rxn2cog.explode('COG description').reset_index(drop=True)
+
+    best_kcats_with_cog = pd.merge(best_individual_df, gene2rxn2cog[['Reactions', 'COG description']],
+                                   left_on='rxn_id', right_on='Reactions', how='left')
+    best_kcats_with_cog = best_kcats_with_cog.drop_duplicates(
+        ['rxn_id', 'enzyme_id', 'r_squared', 'COG description']).reset_index(drop=True)
+    total_change_per_iteration = best_kcats_with_cog.groupby(['alternative'])['kcat_change'].sum().abs().reset_index()
+    return pd.merge(best_kcats_with_cog, total_change_per_iteration,
+                             on=['alternative'])
+
+def summarize_and_pivot_cog_info_df_to_long(cog_info_relative: pd.DataFrame):
+    # Step 1: Compute the sums of positive and negative changes
+    cog_info_relative['positive_change'] = cog_info_relative['kcat_change'].apply(lambda x: x if x > 0 else 0)
+    cog_info_relative['negative_change'] = cog_info_relative['kcat_change'].apply(lambda x: x if x < 0 else 0)
+
+    # Sum of changes grouped by alternative and COG description
+    cog_summary = cog_info_relative.groupby(['COG description', 'alternative']).agg({
+        'positive_change': 'sum',
+        'negative_change': 'sum'
+    }).reset_index()
+
+    # Reshape the data for plotting
+    cog_summary_long = pd.melt(
+        cog_summary,
+        id_vars=['COG description', 'alternative'],
+        value_vars=['positive_change', 'negative_change'],
+        var_name='Change Type',
+        value_name='Change'
+    )
+    return cog_summary_long
+
+def create_barplot(cog_summary_long:pd.DataFrame,
+                   plotting_threshold=0.5,
+                   bar_width=0.2,spacing_factor = 3,  # Increase spacing
+                   fontsize = 15):
+    # only plot the most important cogs
+    cog_summary_long = cog_summary_long.groupby('COG description').filter(
+        lambda x: x['Change'].abs().sum() > plotting_threshold
+    )
+
+    # Sort COG descriptions by total absolute change to get a nice descending plot
+    cog_summary_long['abs_change'] = cog_summary_long['Change'].abs()  # Compute absolute changes
+    cog_order = sorted(
+        set(cog_summary_long['COG description']),
+        key=lambda x: abs(cog_summary_long[cog_summary_long['COG description'] == x]['Change']).sum(),
+        reverse=False
+    )
+
+    #create the plot
+    fig, ax = plt.subplots(figsize=(12, len(cog_summary_long['COG description'].unique()) * 0.5))
+
+    # Define sorted y positions
+    y_positions = np.arange(0, len(cog_order) * spacing_factor, spacing_factor)
+
+    # Get unique alternatives and define a color mapping using coolwarm
+    alternatives = cog_summary_long['alternative'].unique()
+    norm = mcolors.Normalize(vmin=min(alternatives), vmax=max(alternatives))  # Normalize alternative values
+    alternative_colors = {alt: cm.coolwarm(norm(alt)) for alt in alternatives}  # Assign colors from coolw
+
+    # Plot each alternative separately
+    for i, alt in enumerate(cog_summary_long['alternative'].unique()[::-1]):
+        alt_data = cog_summary_long[cog_summary_long['alternative'] == alt]
+
+        # Get y positions for existing data points
+        y_pos_alt = [y_positions[cog_order.index(cog)] for cog in alt_data['COG description'] if cog in cog_order]
+
+        # Separate positive and negative changes
+        positive_data = alt_data[alt_data['Change'] > 0].reset_index()
+        negative_data = alt_data[alt_data['Change'] < 0].reset_index()
+
+        # Apply dodge effect by shifting bars horizontally
+        # Ensure the number of y-positions matches the data length
+        y_pos_positive = np.array(y_pos_alt[:len(positive_data)]) + (i - len(alternatives) / 2) * bar_width
+        y_pos_negative = np.array(y_pos_alt[:len(negative_data)]) + (i - len(alternatives) / 2) * bar_width
+
+        # Plot positive changes
+        ax.barh(y_pos_positive, positive_data['Change'].values,
+                color=alternative_colors[alt], label=f'Alternative {alt}',
+                height=bar_width, align='center')
+
+        # Plot negative changes
+        ax.barh(y_pos_negative, negative_data['Change'].values,
+                color=alternative_colors[alt], height=bar_width, align='center')
+
+    # Add a vertical reference line at 0
+    ax.axvline(0, color='black', linestyle='--', linewidth=1)
+
+    # make x-axis logaritmic
+    # ax.set_xscale('symlog')
+    ax.set_xlim([-2.5 * 1e9, 2.5 * 1e9])
+
+    # Adjust y-axis labels
+    ax.set_yticks(y_positions)
+    ax.set_yticklabels([COG_MAPPER[cog] for cog in cog_order], fontsize=fontsize)  # Set labels in sorted order
+
+    # Label axes
+    ax.set_xlabel(r'$\sum \frac{k_{cat,new}-k_{cat,old}}{k_{cat,old}}$ ', fontsize=fontsize * 1.5)
+    ax.set_ylabel('COG Description', fontsize=fontsize * 1.5)
+
+    # Add legend (ensuring all alternatives are included)
+    handles, labels = ax.get_legend_handles_labels()
+    unique_labels = dict(zip(labels, handles))
+    ax.legend(unique_labels.values(), unique_labels.keys(), title='Alternative model', bbox_to_anchor=(1.05, 1), loc='upper left')
+
+    # Show plot
+    plt.tight_layout()
+    return fig, ax
+
+def recreate_progress_plot(best_indiv_files:list[str],
+                           labels:list[str],
+                           legend:bool = True,
+                           ):
+    FIGWIDTH = 12
+    FIGHEIGHT = 12
+    FONTSIZE = 20
+
+    j=0
+
+    parametrizer, substrate_rates = set_up_ecoli_pam_parametrizer_and_get_substrate_uptake_rates()
+
+    substrate_rates = sorted(substrate_rates)
+    fig, axs = plot_valid_data(parametrizer, fontsize=FONTSIZE)
+    print('Run reference simulations')
+    fluxes, _ = parametrizer.run_simulations_to_plot(substrate_uptake_id='EX_glc__D_e',
+                                                                   substrate_rates = substrate_rates,
+                                                                   sensitivity = False)
+    fig, axs = plot_simulation(fig, axs, fluxes, [abs(rate) for rate in substrate_rates],
+                               parametrizer.validation_data.get_by_id('EX_glc__D_e')._reactions_to_plot,
+                               iteration=0, color='black')
+
+    for file, label in zip(best_indiv_files, labels):
+        j +=1
+        print('\nAlternative ', label, ' from file ', file)
+        parametrizer.pamodel = create_pamodel_from_diagnostics_file(file,
+                                                                    parametrizer._pamodel.copy(copy_with_pickle = True))
+        fluxes, _ = parametrizer.run_simulations_to_plot(substrate_uptake_id='EX_glc__D_e',
+                                                                       substrate_rates=substrate_rates,
+                                                                       sensitivity=False)
+        fig, axs = plot_simulation(fig, axs, fluxes, [abs(rate) for rate in substrate_rates],
+                                   parametrizer.validation_data.get_by_id('EX_glc__D_e')._reactions_to_plot,
+                                   iteration=j + 1, max_iteration=len(best_indiv_files), label = label)
+
+
+    lines, labels = fig.axes[1].get_legend_handles_labels()
+
+    fig.set_figwidth(FIGWIDTH)
+    fig.set_figheight(FIGHEIGHT)
+    if legend:
+        fig.legend(lines, labels, loc='upper left', bbox_to_anchor=(0.1, 0.99), frameon=False,
+                   fontsize=FONTSIZE - 5)
+    fig.tight_layout()
+    return fig, axs
+
+def set_up_ecoli_pam_parametrizer_and_get_substrate_uptake_rates() -> Tuple:
+    kwargs = {'min_substrate_uptake_rate':-12,
+              'max_substrate_uptake_rate': -0.1,
+              'kcat_increase_factor': 3}
+    return set_up_pam_parametrizer_and_get_substrate_uptake_rates(set_up_pamparametrizer,
+                                                           kwargs)
+
+def main():
+    NUM_ALT_MODELS = 1
+    PARAM_FILE_ORI = os.path.join('Results', '1_preprocessing',
+                                  'proteinAllocationModel_iML1515_EnzymaticData_250225.xlsx')
+    PARAM_FILE_PREPROC = os.path.join('Results', '2_parametrization',
+                                      'proteinAllocationModel_iML1515_EnzymaticData_multi.xlsx')
+
+    MODEL_FILE = os.path.join('Models', 'iML1515.xml')
+
+    RESULT_KCAT_HISTOGRAM_PATH = os.path.join('Results', '3_analysis', 'aes_histogram_iML1515.png')
+
+    diagnostic_files = [os.path.join('Results', '2_parametrization', 'diagnostics',
+                                     f'pam_parametrizer_diagnostics_{file_nmbr}.xlsx') for file_nmbr in
+                        range(1, NUM_ALT_MODELS + 1)]
+    parameter_files = [os.path.join('Results', '3_analysis', 'parameter_files',
+                                    f'proteinAllocationModel_EnzymaticData_{model}.xlsx') for model in
+                       range(1, NUM_ALT_MODELS + 1)]
+
+    #create a pretty figure
+    fig = plt.figure(figsize=(10, 12))
+
+    # Outer GridSpec: 2 rows (80% heatmaps, 20% colorbar)
+    gs_main = gridspec.GridSpec(2, 1, height_ratios=[2, 3], wspace=0)
+
+    gs_inner_top = gridspec.GridSpecFromSubplotSpec(2, 2, subplot_spec=gs_main[0])
+    line, line_ax = recreate_progress_plot(diagnostic_files,
+                                           labels=[f'Alternative {i}' for i in range(1, NUM_ALT_MODELS + 1)])
+    for i, ax in enumerate(line_ax):
+        if i>2:
+            ax1_new = fig.add_subplot(gs_inner_top[i, 0])
+        else:
+            ax1_new = fig.add_subplot(gs_inner_top[1, i-2])
+        ax1_new.set_position(ax.get_position(line))  # Reposition into GridSpec
+
+    # Inner GridSpec: 2 columns
+    gs_inner = gridspec.GridSpecFromSubplotSpec(1, 2, subplot_spec=gs_main[1],
+                                                width_ratios=[2, 1])
+    fig.add_subplot(gs_inner[0])
+    hist, hist_ax = create_kcat_histogram_old_vs_new([PARAM_FILE_ORI,
+                                                      PARAM_FILE_PREPROC] + parameter_files,
+                                                     result_fig_file=RESULT_KCAT_HISTOGRAM_PATH,
+                                                     label_names=['GotEnzymes', 'After preprocessing'] \
+                                                                 + [f'Alternative {i}' for i in
+                                                                    range(1, NUM_ALT_MODELS + 1)],
+                                                     legend=False)
+    hist_ax.set_position(gs_inner[0].get_position(fig))
+
+    fig.add_subplot(gs_inner[1])
+    bar, bar_ax = create_kcat_change_per_cog_barplot(PARAM_FILE_PREPROC,
+                                                     MODEL_FILE,
+                                                     diagnostic_files)
+    bar_ax.set_position(gs_inner[1].get_position(fig))
+    plt.savefig(os.path.join('Figures', 'Figure1_parametrization_outcome.png'))
+
+if __name__ == '__main__':
+    main()
---
+++ Figures/SuppFigure3_iml1515_flux_distribution.py
@@ -0,0 +1,86 @@
+import matplotlib.pyplot as plt
+from matplotlib.colors import to_hex
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import os
+
+from PAModelpy.utils import set_up_pam
+from Figures.Figure1_iml1515_kcat_analysis import calculate_distribution_statistics
+
+
+MODEL_FILE = os.path.join('Models', 'iML1515.xml')
+SUBSTRATE_ID = 'EX_glc__D_e'
+
+def create_flux_histogram_old_vs_new(data_file_paths: list[pd.DataFrame],
+                                     label_names:list[str],
+                                     result_fig_file: str,
+                                     other_colors = {'GotEnzymes': 'grey', 'After preprocessing': 'black'},
+                                     cumulative:bool=False):
+    fig, ax = plt.subplots()
+    n_bins = 50
+    i = 0
+    cmap = plt.get_cmap("coolwarm")
+
+    for label, data_file_path in zip(label_names, data_file_paths):
+        print('------------------------------------------------------------------------')
+        print('Analyzing flux distribution of model', label, '\n')
+        model = set_up_pam(pam_info_file=data_file_path,
+                           model=MODEL_FILE,
+                           sensitivity=False)
+        model.change_reaction_bounds(SUBSTRATE_ID, -11,0)
+        solution = model.optimize()
+
+        fluxes = [abs(flux) for flux in solution.fluxes.values if flux!=0]
+        print(
+            f'Model {label} has:\n \ta growth rate of:\t\t{model.objective.value} h-1 with 11 mmol_glc/gCDW/h \n '
+            f'\tMedian fluxes:\t\t\t{np.median(fluxes)} mmol/gCDW/h\n\tMean fluxes:\t\t\t{np.mean(fluxes)} mmol/gCDW/h')
+
+        hist, bins = np.histogram(fluxes, bins=n_bins)
+        logbins = np.logspace(np.log10(bins[0]), np.log10(bins[-1]), len(bins))
+
+        if label in other_colors.keys():
+            color = other_colors[label]
+        else:
+            i += 1
+            color = to_hex(cmap(i / (len(data_file_paths) - len(other_colors))))
+        kwargs = {'cumulative':cumulative}
+        if not cumulative:
+            kwargs = {**kwargs, 'fill': True,  'alpha':0.5}
+
+        bin_heights, bin_borders, _ = ax.hist(fluxes, bins=logbins, histtype='step',
+                                              stacked = True, label = label, color = color,
+                                              **kwargs)
+
+        i += 1
+        calculate_distribution_statistics(bin_heights, bin_borders)
+
+
+    # plt.yscale('log')
+    plt.ylabel('Frequency')
+    plt.xscale('log')
+    plt.xlabel('Flux [mmol/gCDW/h]')
+
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(result_fig_file)
+
+if __name__ == '__main__':
+    NUM_ALT_MODELS = 8
+    PARAM_FILE_ORI = os.path.join('Results', '1_preprocessing',
+                                  'proteinAllocationModel_iML1515_EnzymaticData_250225.xlsx')
+    PARAM_FILE_PREPROC = os.path.join('Results', '2_parametrization',
+                                      'proteinAllocationModel_iML1515_EnzymaticData_multi.xlsx')
+
+    other_files = list()
+    for file_nmbr in range(1, NUM_ALT_MODELS + 1):
+        suffix = f'iML1515_{file_nmbr}'
+
+        other_files += [os.path.join('Results', '3_analysis', 'parameter_files',
+                                     f'proteinAllocationModel_EnzymaticData_{suffix}.xlsx')]
+
+    create_flux_histogram_old_vs_new([PARAM_FILE_ORI,
+                                      PARAM_FILE_PREPROC] + other_files,
+                                     label_names=['GotEnzymes', 'After preprocessing'] \
+                                                 + [f'alternative {i}' for i in range(1, NUM_ALT_MODELS + 1)],
+                                     cumulative=True)
