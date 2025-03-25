c8eb1d7b2852dbdc88aeb92c317475be44d2c52b
samira.vandenbogaard <samira.vandenbogaard@gmail.com>
2025-03-25 16:27:31 +0100

    added fluxomics data for cglutanicum

--- Scripts/i3_analysis/metabolic_flux_distribution_vs_exp.py
+++ Scripts/i3_analysis/metabolic_flux_distribution_vs_exp.py
@@ -3,18 +3,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from typing import List
+
+from PAModelpy import PAModel
+from PAModelpy.utils import set_up_pam
 
 from Modules.utils.pam_generation import setup_pputida_pam
 from Modules.utils.pam_generation import create_pamodel_from_diagnostics_file
 from Modules.utils.pamparametrizer_analysis import get_results_from_simulations
 
-NUM_MODELS = 5
-
-PAM_KCAT_FILES_IJN = [os.path.join('Results', '2_parametrization', 'diagnostics',
-                               f'pam_parametrizer_diagnostics_iJN1463_{file_nmbr}.xlsx') for file_nmbr in
-                  range(1, NUM_MODELS + 1)]
-
-PPUTIDA_PHENOTYPE_FILE_PATH = os.path.join('Data', 'Pputida_phenotypes', 'pputida_phenotypes.xlsx')
 RXNS_TO_VALIDATE = {'Peripheral': ['GLCDpp', 'GAD2ktpp','GNK', '2DHGLCK', 'PGLCNDH'],
                     'EMP': ['HEX1','PGI', 'FBA', 'FBP', 'GAPD', 'PGK', 'PGM', 'ENO', 'PYK'],
                     'ED': ['EDD','EDA'],
@@ -27,21 +24,20 @@ RXNS_TO_VALIDATE = {'Peripheral': ['GLCDpp', 'GAD2ktpp','GNK', '2DHGLCK', 'PGLCN
 rxns_to_save = []
 for rxns in RXNS_TO_VALIDATE.values(): rxns_to_save+=rxns
 
-def get_simulated_fluxes_for_rxns():
-    mfa_data = pd.read_excel(PPUTIDA_PHENOTYPE_FILE_PATH,
-                             sheet_name='fluxomics_glucose').iloc[0]  # first row has most relevant data
-    flux_df = pd.DataFrame(mfa_data[rxns_to_save]).T
-    flux_df['model'] = 'Nikel, et al (2015)'
-    pam = setup_pputida_pam(sensitivity=False)
+def get_simulated_fluxes_for_rxns(mfa_data:pd.Series,
+                                  pam:PAModel,
+                                  pam_kcat_files:List[str]):
+    reactions_to_plot = [rxn for rxn in rxns_to_save if rxn in mfa_data]
+    flux_df = pd.DataFrame(mfa_data[reactions_to_plot]).T
 
     fluxes = {'GotEnzymes': get_results_from_simulations(pam,
                                                          substrate_rates=[[mfa_data['EX_glc__D_e']]],
-                                                         fluxes_to_save=rxns_to_save,
+                                                         fluxes_to_save=reactions_to_plot,
                                                          transl_sector_config=False
                                                          )['fluxes']
               }
 
-    for i, alternative_file in enumerate(PAM_KCAT_FILES_IJN):
+    for i, alternative_file in enumerate(pam_kcat_files):
         alt_pam = create_pamodel_from_diagnostics_file(
             alternative_file,
             pam.copy(copy_with_pickle=True),
@@ -57,14 +53,14 @@ def get_simulated_fluxes_for_rxns():
         df['model'] = model
         flux_df = pd.concat([flux_df, df])
 
-    flux_df.to_excel('Results/3_analysis/mfa_experiment_pputida.xlsx')
     return flux_df
 
-def plot_flux_heatmap_for_pathways(flux_df:pd.DataFrame):
+def plot_flux_heatmap_for_pathways(flux_df:pd.DataFrame,
+                                   result_fig_path:str):
     flux_df = flux_df.drop(['substrate_id', 'substrate'], axis=1).set_index('model').astype(float)
 
     # Create heatmap
-    fig, ax = plt.subplots(figsize=(20, 20))
+    fig, ax = plt.subplots(figsize=(20, 12))
     sns.heatmap(flux_df, annot=True, cmap="coolwarm", fmt=".2f",
                 ax=ax, cbar_kws={'label': r'flux [mmol/$\text{g}_{\text{CDW}}$/h]'}
                 )
@@ -102,12 +98,52 @@ def plot_flux_heatmap_for_pathways(flux_df:pd.DataFrame):
     plt.xlabel("Reaction")
     plt.ylabel("Model")
     plt.tight_layout()
-    plt.savefig(os.path.join('Results', '3_analysis', 'Metabolic_pathways_pputida.png'))
+    plt.savefig(result_fig_path)
+
+def main_iJN1463():
+    NUM_MODELS = 5
+    PAM_KCAT_FILES_IJN = [os.path.join('Results', '2_parametrization', 'diagnostics',
+                                       f'pam_parametrizer_diagnostics_iJN1463_{file_nmbr}.xlsx') for file_nmbr in
+                          range(1, NUM_MODELS + 1)]
+    PPUTIDA_PHENOTYPE_FILE_PATH = os.path.join('Data', 'Pputida_phenotypes', 'pputida_phenotypes.xlsx')
+
+    mfa_data = pd.read_excel(PPUTIDA_PHENOTYPE_FILE_PATH,
+                             sheet_name='fluxomics_glucose').iloc[0]  # first row has most relevant data
+    mfa_data['model'] = 'Nikel, et al (2015)'
+    pam = setup_pputida_pam(sensitivity=False)
+
+    flux_df = get_simulated_fluxes_for_rxns(mfa_data,
+                                            pam,
+                                            PAM_KCAT_FILES_IJN)
+    plot_flux_heatmap_for_pathways(flux_df,
+                                   os.path.join('Results', '3_analysis', 'Metabolic_pathways_pputida.png'))
+
+
+def main_iML1515():
+    NUM_MODELS = 8
+    PAM_KCAT_FILES_IML = [os.path.join('Results', '2_parametrization', 'diagnostics',
+                                       f'pam_parametrizer_diagnostics_iML1515_{file_nmbr}.xlsx') for file_nmbr in
+                          range(1, NUM_MODELS + 1)]
+
+    mfa_data = pd.read_excel(os.path.join('Data', 'Ecoli_phenotypes', 'fluxomics_datasets_gerosa.xlsx'))
+    mfa_data_glc = mfa_data[mfa_data.condition == 'Glucose'][['reaction', 'measured']].set_index('reaction').to_series()
+    mfa_data_glc['model'] = 'Gerosa, et al (2015)'
+    pam = set_up_pam(os.path.join('Results',
+                                  '2_parametrization',
+                                  'proteinAllocationModel_iML1515_EnzymaticData_multi.xlsx'
+                                  )
+                     ,sensitivity=False
+                     )
+
+    flux_df = get_simulated_fluxes_for_rxns(mfa_data_glc,
+                                            pam,
+                                            PAM_KCAT_FILES_IML)
+    plot_flux_heatmap_for_pathways(flux_df,
+                                   os.path.join('Results', '3_analysis', 'Metabolic_pathways_ecoli.png'))
 
 
 if __name__ == '__main__':
-    flux_df = get_simulated_fluxes_for_rxns()
-    plot_flux_heatmap_for_pathways(flux_df)
+    main_iML1515()
 
 
 
