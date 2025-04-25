c4eda3ebc810bb5e10a80b496c55d69f5160306c
samira.vandenbogaard <samira.vandenbogaard@gmail.com>
2025-04-25 12:13:45 +0200

    defined sensitivity of protein sector parameter determination

--- tests/regression_tests/test_pam_parametrizer/test_pam_parametrizer_sector_adjustment.py
+++ tests/regression_tests/test_pam_parametrizer/test_pam_parametrizer_sector_adjustment.py
@@ -16,5 +16,7 @@ def test_if_unused_protein_sector_is_configured_correctly_by_pamparametrizer():
     unused_enzymes_sut = sut.validation_data.EX_glc__D_e.sector_configs['UnusedEnzymeSector']
 
     # Assert
-    assert reference_unused_enzyme_sector.at['ups_0', 'Value'] == pytest.approx(unused_enzymes_sut['intercept'])
-    assert reference_unused_enzyme_sector.at['ups_mu', 'Value'] == pytest.approx(unused_enzymes_sut['slope'])
+    assert reference_unused_enzyme_sector.at['ups_0', 'Value'] == pytest.approx(unused_enzymes_sut['intercept'],
+                                                                                abs = 1e-3)
+    assert reference_unused_enzyme_sector.at['ups_mu', 'Value'] == pytest.approx(unused_enzymes_sut['slope'],
+                                                                                 abs = 1e-3)
