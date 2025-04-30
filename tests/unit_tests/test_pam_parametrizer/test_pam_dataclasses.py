ce258478e5b15a2a0a88c3471b35d0e38dd9bf35
samira.vandenbogaard <samira.vandenbogaard@gmail.com>
2025-04-30 10:54:27 +0200

    made unit test more general to avoid memory copies in all data_objects

--- tests/unit_tests/test_pam_parametrizer/test_pam_dataclasses.py
+++ tests/unit_tests/test_pam_parametrizer/test_pam_dataclasses.py
@@ -1,7 +1,17 @@
-from Modules.PAM_parametrizer.PAM_data_classes import ParametrizationResults
+import pytest
+import pandas as pd
+from typing import Callable
 
-def test_if_ParametrizationResults_is_initialized_properly():
-    obj1 = ParametrizationResults(substrate_uptake_reactions=['EX_glc__D_e'])
-    obj2 = ParametrizationResults(substrate_uptake_reactions=['EX_glc__D_e'])
+from Modules.PAM_parametrizer.PAM_data_classes import ParametrizationResults, SectorConfig, ValidationData, FluxResults
+
+@pytest.mark.parametrize('object, kwargs',[
+    (ParametrizationResults, {'substrate_uptake_reactions':['EX_glc__D_e']}),
+    (SectorConfig, {'slope':1, 'intercept':1, 'sectorname': 'TranslationalProteinSector', 'substrate_range':[0,1]}),
+    (ValidationData, {'valid_data':pd.DataFrame(),'id': 'EX_glc__D_e',  'validation_range': []}),
+    (FluxResults, {'id': 'EX_glc__D_e'})
+])
+def test_if_ParametrizationResults_is_initialized_properly(object:Callable, kwargs):
+    obj1 = object(**kwargs)
+    obj2 = object(**kwargs)
     for attr in obj1.__dataclass_fields__:
-        assert id(getattr(obj1, attr)) == id(getattr(obj2, attr),f"Warning: Field '{attr}' is shared between instances!"
+        assert id(getattr(obj1, attr)) == id(getattr(obj2, attr)),f"Warning: Field '{attr}' is shared between instances!"
