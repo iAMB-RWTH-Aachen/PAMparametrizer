70ca8fc00dd13c0afe80a4a70b9a645661707d59
samira.vandenbogaard <samira.vandenbogaard@gmail.com>
2025-06-06 09:55:55 +0300

    made sure similar isozymes are not messing up testing of right output for a reaction

--- tests/unit_tests/test_utils/test_sector_config_functions.py
+++ tests/unit_tests/test_utils/test_sector_config_functions.py
@@ -4,6 +4,7 @@ import os
 
 from PAModelpy.utils.pam_generation import set_up_pam
 from Modules.utils.sector_config_functions import run_simulations, get_translational_sector_config
+from Modules.utils.pam_generation import _extract_reaction_id_from_catalytic_reaction_id
 from Scripts.pam_generation import setup_toy_pam
 
 def test_if_sector_config_run_simulations_gives_same_results_as_running_simulations():
@@ -19,25 +20,11 @@ def test_if_sector_config_run_simulations_gives_same_results_as_running_simulati
     for fluxes in fluxes_to_test:
         pam.change_reaction_bounds(substr_upt_id, fluxes[substr_upt_id], 0)
         sol = pam.optimize()
+
         assert pam.objective.value > 0
-        assert all([solf == pytest.approx(testf, rel=1e-3) for solf, testf in zip(sol.fluxes, fluxes)])
-
-
-def test_if_sector_config_run_simulations_gives_same_results_as_running_simulations():
-    # Arrange
-    pam = set_up_ecoli_pam(sensitivity = False)
-    substrate_rates = np.arange(-11,-5,1)
-    substr_upt_id = 'EX_glc__D_e'
-
-    # Act
-    fluxes_to_test = run_simulations(pam, substrate_rates, substr_upt_id)
-
-    # Assert
-    for fluxes in fluxes_to_test:
-        pam.change_reaction_bounds(substr_upt_id, fluxes[substr_upt_id], 0)
-        sol = pam.optimize()
-        assert pam.objective.value > 0
-        assert all([solf == pytest.approx(testf, rel=1e-3) for solf, testf in zip(sol.fluxes, fluxes)])
+        for flux_id, testf in fluxes.items():
+            rxn_id = _extract_reaction_id_from_catalytic_reaction_id(flux_id) #to avoid clashed with very similar isozymes
+            assert sol[rxn_id] != pytest.approx(fluxes[rxn_id], rel=1e-3)
 
 
 def test_get_translational_sector_config_returns_sector_correctly():
