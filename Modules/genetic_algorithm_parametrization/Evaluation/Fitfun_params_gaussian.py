made sure units for diffusion limit are correct, where in per second, now in per hour and corrected for reciprocal in coefficient

Modules/genetic_algorithm_parametrization/Evaluation/Fitfun_params_gaussian.py
@@ -23,7 +23,7 @@ from Modules.utils.sector_config_functions import change_sector_parameters_with_
 # set standard paths
 FILE_PATH = Path(abspath(dirname(__file__)))
 DATA_PATH = FILE_PATH.parents[0].joinpath("Data")
-DIFUSSIONLIMIT = 1e6
+DIFUSSIONLIMIT = 1e6 * 3600 # 1e6 [1/s] *3600 [s/h]
 
 # seed random number generator
 random.seed()
@@ -161,7 +161,7 @@ class FitnessEvaluation():
         if kcat_list == []:
             kcats = np.random.lognormal(mean = np.log10(self.KCAT_MU), sigma = np.log10(self.KCAT_SIGMA), size = self.NUM_KCATS)
             # individual.kcat_list = list(kcats)
-            kcats = [kcat if kcat < DIFUSSIONLIMIT else DIFUSSIONLIMIT for kcat in kcats]
+            kcats = [kcat if 1/kcat > DIFUSSIONLIMIT else 1/DIFUSSIONLIMIT for kcat in kcats]
             return kcats
 
         for i, kcat in enumerate(kcat_list):
@@ -392,7 +392,7 @@ class FitnessEvaluation():
             # loc = mean, scale = sd, sd is defined as kcat/10 to make sure sd is in the same order of magntitude
             # as the kcat is
             new_kcat = float(np.random.normal(loc=kcat, scale=stdev))
-            return new_kcat if new_kcat < DIFUSSIONLIMIT else DIFUSSIONLIMIT
+            return new_kcat if 1/new_kcat < DIFUSSIONLIMIT else 1/DIFUSSIONLIMIT
 
 
     def _change_kcat_values_for_individual(self, individual, kcat_values: list = []):

Modules/genetic_algorithm_parametrization/Evaluation/Fitfun_params_uniform.py
@@ -14,12 +14,11 @@ from pathlib import Path
 import os
 from os.path import dirname, abspath
 
-from .Fitfun_params_gaussian import FitnessEvaluation
+from .Fitfun_params_gaussian import FitnessEvaluation, DIFUSSIONLIMIT
 
 # set standard paths
 FILE_PATH = Path(abspath(dirname(__file__)))
 DATA_PATH = FILE_PATH.parents[0].joinpath("Data")
-DIFUSSIONLIMIT = 1e6
 
 # seed random number generator
 random.seed()
@@ -67,7 +66,7 @@ class FitnessEvaluation(FitnessEvaluation):
 
         if kcat_list == []:
             kcats = [kcat
-                     if kcat<DIFUSSIONLIMIT else DIFUSSIONLIMIT
+                     if 1/kcat<DIFUSSIONLIMIT else 1/DIFUSSIONLIMIT
                      for kcat in np.random.lognormal(mean = np.log10(self.KCAT_MU),
                                                      sigma = np.log10(self.KCAT_SIGMA),
                                                      size = self.NUM_KCATS)
@@ -112,7 +111,7 @@ class FitnessEvaluation(FitnessEvaluation):
             # scale needs to be positive, so prevent negative values
             # loc = mean, scale = sd, sd is defined as kcat/10 to make sure sd is in the same order of magntitude
             # as the kcat is
-            max_kcat = kcat*2 if kcat*2<DIFUSSIONLIMIT else DIFUSSIONLIMIT
+            max_kcat = kcat*2 if 1/(kcat*2)<DIFUSSIONLIMIT else 1/DIFUSSIONLIMIT
             return float(np.random.uniform(0,max_kcat))
 
     def _mut_kcat_uniform(self, kcat_list:list, indpb:float):
@@ -127,7 +126,7 @@ class FitnessEvaluation(FitnessEvaluation):
         if random.random() < indpb:
             new_kcats = []
             for kcat in kcat_list:
-                max_kcat = kcat*2 if kcat * 2 < DIFUSSIONLIMIT else DIFUSSIONLIMIT
+                max_kcat = kcat*2 if 1/(kcat * 2) < DIFUSSIONLIMIT else 1/DIFUSSIONLIMIT
                 new_kcats.append(np.random.uniform(0, max_kcat))
         else:
             new_kcats = kcat_list